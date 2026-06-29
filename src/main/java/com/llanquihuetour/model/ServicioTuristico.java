5c7e3256feaf73c6cb81c9d7b13a56e9d327d198
Yerko Cortés Baeza <yerkodigo@gmail.com>
2026-06-29 19:45:26 -0400

    Quita metodos getter y setter que no están siendo utilizados

--- src/main/java/com/llanquihuetour/model/ServicioTuristico.java
+++ src/main/java/com/llanquihuetour/model/ServicioTuristico.java
@@ -11,22 +11,6 @@ public class ServicioTuristico {
         this.duracionHoras = duracionHoras;
     }
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public int getDuracionHoras() {
-        return duracionHoras;
-    }
-
-    public void setDuracionHoras(int duracionHoras) {
-        this.duracionHoras = duracionHoras;
-    }
-
     @Override
     public String toString() {
         return "ServicioTuristico: " + "nombre: '" + nombre + '\'' + ", duracionHoras: " + duracionHoras;
